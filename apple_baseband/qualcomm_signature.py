33c84ce2ec7b9cc26914d500681ccebe6365342a
Rick Mark <rickmark@outlook.com>
2022-05-04 20:08:38 -0700

    text fixtures

---
+++ apple_baseband/qualcomm_signature.py
@@ -0,0 +1,43 @@
+import dataclasses
+import enum
+import io
+import struct
+
+
+# Given the distance between 3/6 it seems this is actually a bitflag of algo / size
+class QualcommHashType(enum.Enum):
+    SHA2_256 = 3
+    SHA2_394 = 6
+
+
+@dataclasses.dataclass
+class QualcommSignatureHeader:
+    HEADER_FORMAT = '<LLLLLLLLLLLL'
+
+    row_count: int  # Zero indicates mastered by ELF
+    hash_type: QualcommHashType
+    storage_offset: int  # Zero makes this mastered by ELF header
+    memory_offset: int  # Zero makes this mastered by the ELF header
+    total_size: int
+    hashes_size: int
+    certificate_offset: int  # 0xFFFFFFFF has special meaning
+    certificate_size: int
+    attestation_offset: int
+    attestation_size: int
+    reserved: int
+    next_header_size: int
+
+    @classmethod
+    def from_bytes(cls, input_data: io.IOBase) -> "QualcommSignatureHeader":
+        return QualcommSignatureHeader(*struct.unpack_from(QualcommSignatureHeader.HEADER_FORMAT, input_data, 0))
+
+
+class QualcommSignature:
+    def __init__(self, data: bytes):
+        self._data = io.BytesIO(data)
+        self._header = QualcommSignatureHeader.from_bytes(self._data)
+        if self._header.next_header_size > 0:
+            self._extra = self._data.read(self._header.next_header_size)
+        else:
+            self._extra = b''
+
---
+++ tests/test_load_signatures.py
@@ -0,0 +1,14 @@
+import pathlib
+import os
+import pytest
+import apple_baseband.qualcomm_signature
+
+SIGNATURES_FOLDER = pathlib.Path(os.path.dirname(__file__)).joinpath('fixtures/signatures')
+
+SIGNATURES = [pytest.param(test_file) for test_file in SIGNATURES_FOLDER.glob('*.b01')]
+
+
+@pytest.mark.parametrize("test_file", SIGNATURES)
+def test_load_signature(test_file: pathlib.Path):
+    print(test_file)
+    signature = apple_baseband.qualcomm_signature.QualcommSignature(test_file.read_bytes())
